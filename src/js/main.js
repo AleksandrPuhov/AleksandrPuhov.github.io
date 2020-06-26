dab4f3a4424bde0004fe65d865c759c67e60f104
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-06-27 00:22:32 +0300

    Work sections add card with animation and codpen iframe

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    const navbar = $('#navbar');
+
+    $(window).scroll(function () {
+        const scrolled = $(this).scrollTop();
+
+        if (scrolled > 0) {
+            navbar.css({
+                backgroundColor: 'rgb(233, 235, 241)',
+                boxShadow: '0px 2px 10px rgb(210, 210, 210)',
+            });
+        } else {
+            navbar.css({
+                backgroundColor: 'transparent',
+                boxShadow: 'none',
+            });
+        }
+    });
+});
